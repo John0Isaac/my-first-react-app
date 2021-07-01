0dfec77651594a8d23e27b636374507d5cf400f0
John0Isaac <johnaziz269@gmail.com>
2021-07-01 04:32:10 +0200

    Props and Keys

--- src/App.js
+++ src/App.js
@@ -1,38 +1,66 @@
 import React from 'react';
 import './App.css';
+import PropTypes from "prop-types";
 
-function Header() {
+function Header(props) {
     return (
         <header>
-            <h1>Eve&apos;s Kitchen</h1>
+            <h1>{props.name}&apos;s Kitchen</h1>
         </header>
     );
 }
 
-function Main() {
+function Main(props) {
     return (
         <section>
-            <p>We serve the most delicious food around</p>
+            <p>We serve the most {props.adjective} food around</p>
+            <ul style={ { textAlign: "left" }}>
+                {props.dishes.map((dish) => (
+                <li key={dish.id}>{dish.title}</li>
+                ))}
+            </ul>
         </section>
     );
 }
 
-function Footer() {
+function Footer(props) {
     return (
         <footer>
-            <p>It&apos;s True</p>
+            <p>Copyrights {props.year}</p>
         </footer>
     );
 }
 
+const dishes = [
+    "Macaroni", 
+    "Rice and Chicken",
+    "Salmon"
+];
+
+const dishObjects = dishes.map((dish, i ) => ({ id: i, title: dish }));
+
 function App() {
     return ( 
         <div className="App">
-            <Header />
-            <Main />
-            <Footer /> 
+            <Header name="John" />
+            <Main adjective="amazing" dishes={dishObjects} />
+            <Footer year={new Date().getFullYear()} /> 
         </div>
     );
+    
 }
+Header.propTypes = {
+    name: PropTypes.any,
+};
+
+Main.propTypes = {
+    adjective: PropTypes.string,
+    dishes: PropTypes.array,
+    dishObjects: PropTypes.object,
+};
+
+Footer.propTypes = {
+    year: PropTypes.any,
+};
 
 export default App;
